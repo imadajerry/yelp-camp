4806a448b6cb1c54ad58d0f234d34f66a292c786
Jerry <jerrylalrinmawia@gmail.com>
2025-06-02 13:43:14 +0530

    Imported multer and tested it out

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 
@@ -9,28 +11,30 @@ const campgrounds = require("../controllers/campgrounds");
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    res.send({
+      body: req.body,
+      file: req.files,
+    });
+  });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
 router
   .route("/:id")
-  .get((campgrounds.showCampground))
+  .get(campgrounds.showCampground)
   .put(
     isLoggedIn,
     isAuthor,
     validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
-  .delete(
-    isLoggedIn,
-    isAuthor,
-    catchAsync(campgrounds.deleteCampground)
-  );
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
