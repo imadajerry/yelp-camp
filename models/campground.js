3382224c25552a941066fe3cd1fc0d195266c075
Jerry <jerrylalrinmawia@gmail.com>
2025-06-11 15:43:51 +0530

    Change image to Images and moved it to its own schema to add "thumbnail" virtual

--- models/campground.js
+++ models/campground.js
@@ -2,21 +2,32 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+})
+
+ImageSchema.virtual("thumbnail").get(function() {
+  return this.url.replace("/upload", "/upload/w_200")
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
+  images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
     author: {
         type: Schema.Types.ObjectId,
-        ref: "User"
+    ref: "User",
     },
-    reviews: [{
+  reviews: [
+    {
         type: Schema.Types.ObjectId,
-        ref: "Review"
-    }]
-})
+      ref: "Review",
+    },
+  ],
+});
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
     if (doc) {
